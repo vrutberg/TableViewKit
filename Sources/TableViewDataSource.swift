9721621cdeffa443387ea20ce19de339158d92d9
Askia Linder <askia.linder@avanza.se>
2018-05-29 10:45:29 +0200

    no reloading of table

--- Sources/TableViewDataSource.swift
+++ Sources/TableViewDataSource.swift
@@ -97,13 +97,7 @@ public class TableViewDataSource: NSObject {
 
         }
 
-        processingOperation.completionBlock = {
-            DispatchQueue.main.sync { [weak self] in
-                print("SYNC!!!!!!!!!!")
-                self?.reloadData(newSections: newSections)
-            }
-            completion()
-        }
+        processingOperation.completionBlock = completion
         processingQueue.cancelAllOperations() // If user spams UI, and toggles state back and forth, make sure to cancel old state changes and just process the most recent complete state
         processingQueue.addOperation(processingOperation)
     }
