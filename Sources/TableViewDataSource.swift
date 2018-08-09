52b5c076c0147d18cf24671e36dcb8f8805fc2f0
Askia Linder <askia.linder@avanza.se>
2018-08-09 10:53:40 +0200

    FF-126: be scrollViewDelegate more future proof

--- Sources/TableViewDataSource.swift
+++ Sources/TableViewDataSource.swift
@@ -16,8 +16,17 @@ public class TableViewDataSource: NSObject {
     /// The current table view sections
     public fileprivate(set) var sections: [TableViewSection]
     fileprivate weak var tableView: UITableView?
-    public weak var scrollViewDelegate: UIScrollViewDelegate?
     fileprivate let processingQueue = OperationQueue()
+
+    public weak var scrollViewDelegate: UIScrollViewDelegate? {
+        didSet {
+            guard let daddy = tableView as UIScrollView? else {
+                assertionFailure("UITableView is not UIScrollView, crazy")
+                return
+            }
+            daddy.delegate = scrollViewDelegate
+        }
+    }
 
     /// The designated initializer
     ///
@@ -326,64 +335,6 @@ extension TableViewDataSource: UITableViewDelegate {
             sections[indexPath].copyAction?(tableView, cell, indexPath)
         } else if action == #selector(UIResponderStandardEditActions.paste(_:)) {
             sections[indexPath].pasteAction?(tableView, cell, indexPath)
-        }
-    }
-
-    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewWillBeginDragging?(scrollView)
-    }
-
-    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewDidScroll?(scrollView)
-    }
-
-    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        scrollViewDelegate?.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
-    }
-
-    public func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewDidZoom?(scrollView)
-    }
-
-    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        scrollViewDelegate?.scrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
-    }
-
-    public func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewWillBeginDecelerating?(scrollView)
-    }
-
-    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewDidEndDecelerating?(scrollView)
-    }
-
-    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewDidEndScrollingAnimation?(scrollView)
-    }
-
-    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return scrollViewDelegate?.viewForZooming?(in: scrollView)
-    }
-
-    public func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-        scrollViewDelegate?.scrollViewWillBeginZooming?(scrollView, with: view)
-    }
-
-    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-        scrollViewDelegate?.scrollViewDidEndZooming?(scrollView, with: view, atScale: scale)
-    }
-
-    public func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
-        return scrollViewDelegate?.scrollViewShouldScrollToTop?(scrollView) ?? false
-    }
-
-    public func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
-        scrollViewDelegate?.scrollViewDidScrollToTop?(scrollView)
-    }
-
-    public func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
-        if #available(iOS 11.0, *) {
-            scrollViewDelegate?.scrollViewDidChangeAdjustedContentInset?(scrollView)
         }
     }
 }
